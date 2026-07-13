37da05c012a3fc8e45d7428f976cb1f69a2cc107
rakesh <rakeshchandra.offcl@gmail.com>
2026-07-14 00:22:18 +0530

    Add Clerk authentication integration and update dependencies

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,13 @@ import { Geist, Geist_Mono, Inter } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
 import { ThemeProvider } from "@/components/provider/theme-provider";
+import {
+	ClerkProvider,
+	SignInButton,
+	SignUpButton,
+	UserButton,
+	Show,
+} from "@clerk/nextjs";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-sans" });
 
@@ -39,14 +46,29 @@ export default function RootLayout({
 			)}
 		>
 			<body className="min-h-full flex flex-col">
-				<ThemeProvider
-					attribute="class"
-					defaultTheme="system"
-					enableSystem
-					disableTransitionOnChange
-				>
-					{children}
-				</ThemeProvider>
+				<ClerkProvider>
+					<ThemeProvider
+						attribute="class"
+						defaultTheme="system"
+						enableSystem
+						disableTransitionOnChange
+					>
+						<header className="flex justify-end items-center p-4 gap-4 h-16">
+							<Show when="signed-out">
+								<SignInButton />
+								<SignUpButton>
+									<button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+										Sign Up
+									</button>
+								</SignUpButton>
+							</Show>
+							<Show when="signed-in">
+								<UserButton />
+							</Show>
+						</header>
+						{children}
+					</ThemeProvider>
+				</ClerkProvider>
 			</body>
 		</html>
 	);
